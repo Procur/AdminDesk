c5a3cfbda237112a62e531104bb8daeb45662aab
Will Ricketts <will@willricketts.com>
2014-07-21 10:24:55 -0400

    test request and response handling

--- api/controllers/DashboardController.js
+++ api/controllers/DashboardController.js
@@ -7,7 +7,16 @@ var rest = require('rest');
 module.exports = {
 
 	dashboard: function(req, res) {
-		res.view();
+		rest({ method: 'GET', path: sails.config.api.host + '/admin/staff'}).then(function(response){
+			var adminList,
+					apiResponse;
+			try {
+				apiResponse = JSON.parse(response.entity);
+			}
+			catch(err) {
+				apiResponse = response.entity;
+			}
+			res.view({ response: apiResponse });
+		});
 	}
-
 };
