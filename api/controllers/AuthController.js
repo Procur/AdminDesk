abb9c0b411c42ea3365c10672df5885bfd4bb240
Will Ricketts <will@willricketts.com>
2014-07-21 11:24:37 -0400

    added helper skeletons and authController

---
+++ api/controllers/AuthController.js
@@ -0,0 +1,16 @@
+/**
+ * AuthController
+ *
+ * @description :: Server-side logic for managing auths
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+var token = require('../helpers/tokenFunctions.js');
+
+module.exports = {
+
+	validateApiToken: function(req, res) {
+
+	}
+
+};
